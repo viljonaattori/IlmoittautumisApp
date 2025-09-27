f36ea734193240f43a60423466d863a1762527ea
Vili Auvinen <2207441@edu.karelia.fi>
2025-09-27 13:55:23 +0300

    ju

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const path = require("path");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 app.use(tokenExtractor);

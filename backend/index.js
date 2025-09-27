18c12ce98f6f130a8704be69de49c142a76c4e21
Vili Auvinen <2207441@edu.karelia.fi>
2025-09-27 09:55:26 +0300

    dist

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const app = require("./app");
 const { ping } = require("./db/pool");
 
-// Jos PORT on annettu .env:ssä, käytä sitä, muuten Renderin omaa process.env.PORT:ia
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, async () => {

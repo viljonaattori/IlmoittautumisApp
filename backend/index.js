e2f28a525b323549d686b5238d2b0d2cf5f742c7
Vili Auvinen <2207441@edu.karelia.fi>
2025-09-27 09:45:04 +0300

    muutoksia

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const app = require("./app");
-const { PORT } = require("./utils/config");
 const { ping } = require("./db/pool");
+
+// Jos PORT on annettu .env:ssä, käytä sitä, muuten Renderin omaa process.env.PORT:ia
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, async () => {
   const now = await ping().catch(() => null);
   console.log(
-    `Server running on port ${PORT}${now ? ` | DB OK @ ${now}` : ""}` // Tietokannan testaus
+    `Server running on port ${PORT}${now ? ` | DB OK @ ${now}` : ""}`
   );
 });

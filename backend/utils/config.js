fa15e31d50b99d48d577aa9b4f1e6a78b887e63a
Vili Auvinen <2207441@edu.karelia.fi>
2025-09-29 12:19:43 +0300

    rajoitettu tietokanta yhteyksien määrää

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -9,7 +9,7 @@ const DB = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT || 10),
+  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT || 5),
 };
 
 const JWT = {
